d4aaf0379b3acbb0d5dd93534028accc0d587db4
A-Wakidou <57793874+A-Wakidou@users.noreply.github.com>
2021-11-29 22:11:59 +0100

    1st api call

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { Home,Account,Explorer } from "./pages"
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -12,50 +11,32 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
-      
       screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-
-          if (route.name === 'Home') {
-            iconName = focused
-              ? 'ios-information-circle'
-              : 'ios-information-circle-outline';
-          } else if (route.name === 'Settings') {
-            iconName = focused ? 'ios-list-box' : 'ios-list';
-          }
-
-          // You can return any component that you like here!
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
         tabBarActiveTintColor: 'tomato',
         tabBarInactiveTintColor: 'gray',
-      })}
-
-      >
-      <Tab.Screen
-        name="Home"
-        component={Home}
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          )}} />
-      <Tab.Screen
-        name="Explorer"
-        component={Explorer}
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="glass" color={color} size={size} />
-          )}} />
-      <Tab.Screen 
-        name="Account"
-        component={Account}
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account" color={color} size={size} />
-          )}} />
+      })}>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            )}} />
+        <Tab.Screen
+          name="Explorer"
+          component={Explorer}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="glass" color={color} size={size} />
+            )}} />
+        <Tab.Screen 
+          name="Account"
+          component={Account}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="account" color={color} size={size} />
+            )}} />
       </Tab.Navigator>
-      <StatusBar style="auto" />
     </NavigationContainer>
   );
 }
--- pages/Home/index.js
+++ pages/Home/index.js
@@ -1,22 +1,43 @@
 import React, {useEffect, useState} from "react"
-import { View,Text } from "react-native"
-import axios from "axios"
+import { View,Text, StyleSheet,FlatList, ActivityIndicator, Image } from "react-native"
 
 const Home = ({navigation}) => {
-  const [data,setData] = useState([])
-  const callAPI = () => {
-    axios.get('https://kitsu.io/api/edge/anime?filter[categories]=adventure')
-      .then(response => {
-        setData(response)
-      })
-      console.log(data)
+
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const getMangas = async () => {
+    try {
+      const response = await fetch('https://kitsu.io/api/edge/anime?filter[text]=cowboy%20bebop');
+      const json = await response.json()
+      console.log(json.data)
+      setData(json.data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   }
-  callAPI()
+
+  useEffect(() => {
+    getMangas()
+  }, [])
+
   return (
-    <View>
-      <Text>{data}</Text>
+    <View style={{ flex:1, padding:24}}>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <div style={{flex:1}}>
+              <Text style={{marginBottom:40}}>{item.attributes.canonicalTitle}</Text>
+              <Image style={{flex:1, width:100, height:100}} source={item.attributes.posterImage.tiny} />
+            </div>
+
+          )}
+        />
+      )}
     </View>
   )
 }
-
 export default Home
